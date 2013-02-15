7964ce79a89b47c457d8bf6ccf09143522424b90
darrylwolfaardt <darryl@herebemagic.com>
2013-02-15 21:30:03 +1300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mova::Application.config.secret_token = 'f56684bd94a0e454a6abdd6f875d007ae17b23e1709f19864f425e25ae5f93402d264738b3731451191f8003268e4b7694c8a42da2cdde4c55812587bdfda4bb'
